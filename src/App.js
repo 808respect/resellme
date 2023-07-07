f89bc2ea478465d8978490f3e026ecacf4976937
REESCOH <respect.r.tsvuura@gmail.com>
2023-07-07 13:40:15 +0200

    done-1

--- src/App.js
+++ src/App.js
@@ -84,8 +84,10 @@ function App() {
               <section id="accordionSection">
                <div className="row">
                     <div className="col-lg-6">
-                         <h2>Frequently Asked Questions</h2>
-                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                         <div id="textContainer">
+                              <h2>Frequently Asked Questions</h2>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                         </div>
                     </div>
                     <div className="col-lg-6">
                          <Accordionne />
